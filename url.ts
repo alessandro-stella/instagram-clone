04c06fbcd0109c2b77a5daa7c92728911f1ad977
Alessandro Stella <alessandro.stella2004@gmail.com>
2023-04-22 18:55:20 +0200

    Adding URL for deployment

--- url.ts
+++ url.ts
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export default dev
     ? "http://localhost:3000"
-    : /* "https://to-do-alessandro-stella.vercel.app/" */ "";
+    : "https://instagram-clone-alessandro-stella.vercel.app/";
